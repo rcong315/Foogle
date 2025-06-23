0dbec77550dd5cef7045c0ccd4559c7dec1fe541
rcong315 <richardcong635@gmail.com>
2025-06-22 19:30:34 -0700

    Init project

---
+++ Foogle/FoogleApp.swift
@@ -0,0 +1,32 @@
+//
+//  FoogleApp.swift
+//  Foogle
+//
+//  Created by Richard Cong on 6/22/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FoogleApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ FoogleTests/FoogleTests.swift
@@ -0,0 +1,17 @@
+//
+//  FoogleTests.swift
+//  FoogleTests
+//
+//  Created by Richard Cong on 6/22/25.
+//
+
+import Testing
+@testable import Foogle
+
+struct FoogleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
